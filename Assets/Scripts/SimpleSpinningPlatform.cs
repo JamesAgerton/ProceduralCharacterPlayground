fb48c20ca8e2890f85196a7fd142bdc0ab5b3b94
James Agerton <jpa234@yahoo.com>
2021-02-26 20:51:59 -0600

    Added a moving platform
    Moving platforms are common game elements, so my character controller should be able to handle that scenario in a satisfying way.

---
+++ Assets/Scripts/SimpleSpinningPlatform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SimpleSpinningPlatform : MonoBehaviour
+{
+    private Rigidbody _body;
+
+    [SerializeField]
+    private float _spinRate = 1f;
+
+    float angle = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle += _spinRate * Time.deltaTime;
+        Quaternion newRotation = Quaternion.Euler(0f, angle, 0f);
+        _body.MoveRotation(newRotation);
+    }
+}
