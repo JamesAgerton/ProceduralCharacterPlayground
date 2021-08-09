f9c6a166de7d5868f2695676cd8946420f62956e
James Agerton <jpa234@yahoo.com>
2021-08-08 23:03:39 -0500

    New IK implementation
    Fixed slope issue thanks to tut found online. But need better animations to test the rotation implementation, impossible to tell if feet are sliding or rising properly because key frames are shit.

--- Assets/ThirdPersonCharacter/Scripts/Animation/CharacterAnimationIK.cs
+++ Assets/ThirdPersonCharacter/Scripts/Animation/CharacterAnimationIK.cs
@@ -8,24 +8,32 @@ namespace ProceduralCharacter.Animation
     {
         #region Variables (PRIVATE)
         Animator _anim;
+        
         [SerializeField]
-        CharacterAnimationController _CAC;
-
-        [SerializeField]
-        bool _IKWeightGizmo = false;
-
+        bool _enableFeetIK = true;
+        [SerializeField, Range(0, 2)]
+        float _raycastStartHeight = 1.14f;
+        [SerializeField, Range(0, 2)]
+        float _raycastDownDistance = 1.5f;
         [SerializeField]
         LayerMask _ground;
+        [SerializeField]
+        float _pelvisOffset = 0f;
+        [SerializeField, Range(0, 1)]
+        float _pelvisUpDownSpeed = 0.28f;
+        [SerializeField, Range(0, 1)]
+        float _feetToIKPositionSpeed = 0.5f;
 
-        [SerializeField, Range(0f, 1f), Tooltip("The difference between the bottom of the model's foot and the IK position.")]
-        float _ankleHeight;
+        public string LeftFootAnimVariableName = "LeftFootCurve";
+        public string RightFootAnimVariableName = "RightFootCurve";
 
-        [SerializeField, Tooltip("Curve used to evaluate foot weight based on CharacterAnimationController:StrideFraction.")]
-        AnimationCurve _IKWeightCurve;
-        [SerializeField, Tooltip("Offset of weight curve from CharacterAnimationController StrideWeightCurve.")]
-        float _offset = 0f;
-        float _leftFootIKWeight = 1f;
-        float _rightFootIKWeight = 1f;
+        public bool UseProIKFeature = false;
+        public bool ShowSolverDebug = true;
+
+        Vector3 _rightFootPosition, _leftFootPosition, _rightFootIKPosition, _leftFootIKPosition;
+        Quaternion _rightFootIKRotation, _leftFootIKRotation;
+        float _lastPelvisPositionY, _lastRightFootPositionY, _lastLeftFootPositionY;
+
         #endregion
 
         #region Properties (PUBLIC)
@@ -33,95 +41,160 @@ namespace ProceduralCharacter.Animation
         #endregion
 
         #region Unity Methods
-        // Start is called before the first frame update
+
         private void Start()
         {
             _anim = GetComponent<Animator>();
-            if (_anim == null)
+            if(_anim == null)
             {
-                Debug.LogError("CharacterAnimationIK must be a component on the same object as the Animator component!", this);
-            }
-
-            if (_CAC == null)
-            {
-                Debug.LogError("CharacterAnimatorIK needs a reference to a CharacterAnimationController component!", this);
+                Debug.LogError("CharacterAnimationIK must be on the same gameobject as the Animator component!", this);
             }
         }
 
-        private void Update()
+        /// <summary>
+        /// Updating the AdjustFeetTarget method and also finding the position of each foot inside the Solver Position.
+        /// </summary>
+        private void FixedUpdate()
         {
-            GetFootWeights();
+            if(!_enableFeetIK) { return; }
+            if(_anim == null) { return; }
+
+            AdjustFeetTarget(ref _rightFootPosition, HumanBodyBones.RightFoot);
+            AdjustFeetTarget(ref _leftFootPosition, HumanBodyBones.LeftFoot);
+
+            //find and raycast to the ground to find positions
+            FeetPositionSolver(_rightFootPosition, ref _rightFootIKPosition, ref _rightFootIKRotation); // handle solver for right foot
+            FeetPositionSolver(_leftFootPosition, ref _leftFootIKPosition, ref _leftFootIKRotation);    // handle solver for left foot
+
         }
 
         private void OnAnimatorIK(int layerIndex)
         {
-            if (_anim != null && _CAC != null)
+            if (!_enableFeetIK) { return; }
+            if(_anim == null) { return; }
+
+            MovePelvisHeight();
+
+            //right foot IK position and rotation -- utilize the pro features here
+            _anim.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1f);
+            if (UseProIKFeature)
             {
-                _anim.SetIKPositionWeight(AvatarIKGoal.LeftFoot, _leftFootIKWeight);
-                _anim.SetIKRotationWeight(AvatarIKGoal.LeftFoot, _leftFootIKWeight);
-
-                _anim.SetIKPositionWeight(AvatarIKGoal.RightFoot, _rightFootIKWeight);
-                _anim.SetIKRotationWeight(AvatarIKGoal.RightFoot, _rightFootIKWeight);
-
-                // Left Foot
-                RaycastHit hitInfo;
-                Ray ray = new Ray(_anim.GetIKPosition(AvatarIKGoal.LeftFoot) + Vector3.up, Vector3.down);
-                //Debug.DrawRay(ray.origin, ray.direction);
-                if (Physics.Raycast(ray, out hitInfo, _ankleHeight + 1.2f, _ground))
-                {
-                    Vector3 footPosition = hitInfo.point;
-                    footPosition.y += _ankleHeight;
-                    _anim.SetIKPosition(AvatarIKGoal.LeftFoot, footPosition);
-                    _anim.SetIKRotation(AvatarIKGoal.LeftFoot, Quaternion.LookRotation(transform.forward, hitInfo.normal));
-                }
-
-                // Right Foot
-                ray = new Ray(_anim.GetIKPosition(AvatarIKGoal.RightFoot) + Vector3.up, Vector3.down);
-                //Debug.DrawRay(ray.origin, ray.direction);
-                if (Physics.Raycast(ray, out hitInfo, _ankleHeight + 1.2f, _ground))
-                {
-                    Vector3 footPosition = hitInfo.point;
-                    footPosition.y += _ankleHeight;
-                    _anim.SetIKPosition(AvatarIKGoal.RightFoot, footPosition);
-                    _anim.SetIKRotation(AvatarIKGoal.RightFoot, Quaternion.LookRotation(transform.forward, hitInfo.normal));
-                }
+                _anim.SetIKRotationWeight(AvatarIKGoal.RightFoot, _anim.GetFloat(RightFootAnimVariableName));
             }
+
+            MoveFeetToIKPoint(AvatarIKGoal.RightFoot, _rightFootIKPosition, _rightFootIKRotation, ref _lastRightFootPositionY);
+
+
+            //left foot IK position and rotation -- utilize the pro features here
+            _anim.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1f);
+            if (UseProIKFeature)
+            {
+                _anim.SetIKRotationWeight(AvatarIKGoal.LeftFoot, _anim.GetFloat(LeftFootAnimVariableName));
+            }
+
+            MoveFeetToIKPoint(AvatarIKGoal.LeftFoot, _leftFootIKPosition, _leftFootIKRotation, ref _lastLeftFootPositionY);
         }
 
         private void OnDrawGizmos()
         {
-            if (_IKWeightGizmo && Application.isPlaying)
-            {
-                Vector3 Rfoot = _anim.GetIKPosition(AvatarIKGoal.RightFoot);
-                Vector3 Lfoot = _anim.GetIKPosition(AvatarIKGoal.LeftFoot);
-
-                Color color = Color.Lerp(Color.red, Color.green, _rightFootIKWeight);
-                Gizmos.color = color;
-                Gizmos.DrawLine(Rfoot, Rfoot + Vector3.up * _rightFootIKWeight * 0.5f);
-
-                color = Color.Lerp(Color.red, Color.green, _leftFootIKWeight);
-                Gizmos.color = color;
-                Gizmos.DrawLine(Lfoot, Lfoot + Vector3.up * _leftFootIKWeight * 0.5f);
-            }
+            
         }
         #endregion
 
         #region Methods
-        void GetFootWeights()
+        /// <summary>
+        /// Moves feet to IK point.
+        /// </summary>
+        /// <param name="foot"></param>
+        /// <param name="positionIKHolder"></param>
+        /// <param name="rotationIKHolder"></param>
+        /// <param name="lastFootPositionY"></param>
+        void MoveFeetToIKPoint(AvatarIKGoal foot, Vector3 positionIKHolder, Quaternion rotationIKHolder, ref float lastFootPositionY)
         {
-            if(_CAC != null)
+            Vector3 targetIKPosition = _anim.GetIKPosition(foot);
+
+            if(positionIKHolder != Vector3.zero)
             {
-                if (_CAC.IsMoving)
-                {
-                    _rightFootIKWeight = _IKWeightCurve.Evaluate((_CAC.StrideFraction + _offset) % 0.5f);
-                    _leftFootIKWeight = _IKWeightCurve.Evaluate((_CAC.StrideFraction + 0.25f + _offset) % 0.5f);
-                }
-                else
-                {
-                    _rightFootIKWeight = 1f;
-                    _leftFootIKWeight = 1f;
-                }
+                targetIKPosition = transform.InverseTransformPoint(targetIKPosition);
+                positionIKHolder = transform.InverseTransformPoint(positionIKHolder);
+
+                float yVariable = Mathf.Lerp(lastFootPositionY, positionIKHolder.y, _feetToIKPositionSpeed);
+                targetIKPosition.y += yVariable;
+
+                lastFootPositionY = yVariable;
+
+                targetIKPosition = transform.TransformPoint(targetIKPosition);
+
+                _anim.SetIKRotation(foot, rotationIKHolder);
             }
+
+            _anim.SetIKPosition(foot, targetIKPosition);
+        }
+
+        /// <summary>
+        /// Moves the height of the pelvis.
+        /// </summary>
+        void MovePelvisHeight()
+        {
+            if (_rightFootIKPosition == Vector3.zero || _leftFootIKPosition == Vector3.zero || _lastPelvisPositionY == 0f)
+            {
+                _lastPelvisPositionY = _anim.bodyPosition.y;
+                return;
+            }
+
+            float lOffsetPosition = _leftFootIKPosition.y - transform.position.y;
+            float rOffsetPosition = _rightFootIKPosition.y - transform.position.y;
+
+            float totalOffset = (lOffsetPosition < rOffsetPosition) ? lOffsetPosition : rOffsetPosition;
+
+            Vector3 newPelvisPosition = _anim.bodyPosition + Vector3.up * totalOffset;
+
+            newPelvisPosition.y = Mathf.Lerp(_lastPelvisPositionY, newPelvisPosition.y, _pelvisUpDownSpeed);
+
+            _anim.bodyPosition = newPelvisPosition;
+
+            _lastPelvisPositionY = _anim.bodyPosition.y;
+        }
+
+        /// <summary>
+        /// Locating the feet position via raycast and then solving.
+        /// </summary>
+        /// <param name="fromSkyPosition">Raycast origin.</param>
+        /// <param name="feetIKPositions"></param>
+        /// <param name="feetIKRotations"></param>
+        void FeetPositionSolver(Vector3 fromSkyPosition, ref Vector3 feetIKPositions, ref Quaternion feetIKRotations)
+        {
+            //raycast handling section
+            RaycastHit feetOutHit;
+
+            if (ShowSolverDebug)
+            {
+                Debug.DrawLine(fromSkyPosition, fromSkyPosition + Vector3.down * (_raycastDownDistance + _raycastStartHeight), Color.yellow);
+            }
+
+            if(Physics.Raycast(fromSkyPosition, Vector3.down, out feetOutHit, _raycastDownDistance + _raycastStartHeight, _ground))
+            {
+                //Find feet ik positions from sky position
+                feetIKPositions = fromSkyPosition;
+                feetIKPositions.y = feetOutHit.point.y + _pelvisOffset;
+                feetIKRotations = Quaternion.FromToRotation(Vector3.up, feetOutHit.normal) * transform.rotation;
+
+                return;
+            }
+
+            feetIKPositions = Vector3.zero; //it didn't work :(
+        }
+
+        /// <summary>
+        /// Adjusts the feet target.
+        /// </summary>
+        /// <param name="feetPositions"></param>
+        /// <param name="foot"></param>
+        void AdjustFeetTarget(ref Vector3 feetPositions, HumanBodyBones foot)
+        {
+            feetPositions = _anim.GetBoneTransform(foot).position;
+            feetPositions.y = transform.position.y + _raycastStartHeight;
+
         }
         #endregion
     }
